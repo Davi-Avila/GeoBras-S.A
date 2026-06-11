dd0568774d9de9a11664ab2779c537f4cacb4437
DAVI DOMINGOS ÁVILA <davi_d_avila@estudante.sesisenai.org.br>
2026-06-10 21:59:41 -0300

    Parte do CRUD da class Obra

--- backend/GeObras/src/main/java/com/example/geobras/controller/ObraController.java
+++ backend/GeObras/src/main/java/com/example/geobras/controller/ObraController.java
@@ -1,4 +1,43 @@
 package com.example.geobras.controller;
 
+import com.example.geobras.dto.ObraRequestDTO;
+import com.example.geobras.dto.ObraResponseDTO;
+import com.example.geobras.service.ObraService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/{obras}")
 public class ObraController {
+    @Autowired
+    private ObraService obraService;
+
+    @GetMapping
+    public List<ObraResponseDTO> listar(){
+        return obraService.listar();
+    }
+
+    @PostMapping
+    public ObraResponseDTO salvar(@RequestBody @Valid ObraRequestDTO dto){
+        return obraService.salvar(dto);
+    }
+
+    @PutMapping("/{idObra}")
+    public ObraResponseDTO atualizar(@PathVariable Long idObra, @RequestBody @Valid ObraRequestDTO dto){
+        return obraService.atualizar(idObra, dto);
+    }
+
+    @DeleteMapping("/{idObra}")
+    public void deletar(@PathVariable Long idObra){
+        obraService.deletar(idObra);
+    }
+
+    @GetMapping("/{idObra}")
+    public ObraResponseDTO buscarPorId(@PathVariable Long idObra){
+        return obraService.buscarPorId(idObra);
+    }
+
 }
--- backend/GeObras/src/main/java/com/example/geobras/service/ObraService.java
+++ backend/GeObras/src/main/java/com/example/geobras/service/ObraService.java
@@ -1,4 +1,43 @@
 package com.example.geobras.service;
 
+import com.example.geobras.dto.ObraResponseDTO;
+import com.example.geobras.exception.RecursoNaoEncontradoException;
+import com.example.geobras.model.Obra;
+import com.example.geobras.repository.ObraRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ObraService {
+    @Autowired
+    private ObraRepository obraRepository;
+
+    public List<ObraResponseDTO> listar(){
+        return obraRepository.findAll()
+                .stream()
+                .map(this::toResponseDTO)
+                .toList();
+    }
+
+    public void deletar(Long idObra){
+        Obra obra = obraRepository.findById(idObra)
+                .orElseThrow(() -> new RecursoNaoEncontradoException("Obra não encontrada"));
+
+        obraRepository.delete(obra);
+    }
+
+    private ObraResponseDTO toResponseDTO(Obra obra){
+        return new ObraResponseDTO(
+                obra.getNomeObra(),
+                obra.getEndereco(),
+                obra.getDataInicio(),
+                obra.getDataFimPrevisto(),
+                obra.getEtapa(),
+                obra.getOrcamento().getOrcamentoTotal(),
+                obra.getCliente().getNomeCliente(),
+                obra.getCliente().getEmail()
+        );
+    }
 }
