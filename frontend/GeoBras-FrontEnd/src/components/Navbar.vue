5d86f3f23ecc246a0483ab1cd328a420e08b9401
DAVI MARTINS CORDEIRO <davi_m_cordeiro@estudante.sesisenai.org.br>
2026-06-17 21:55:34 -0300

    front-end - estrutura inicial

---
+++ frontend/GeoBras-FrontEnd/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav style="background-color: #343a40; display: flex;color: white; flex-direction: row; align-items: center; padding: 0.5rem 1rem; height: 60px;">
+      
+    
+
+      <RouterLink to="/" style="color: white; text-decoration: none; font-size: 1.5rem; font-weight: bold;">
+        
+        GeoBras
+      </RouterLink>
+
+
+            <RouterLink to="/" style="color: white; text-decoration: none; font-size: 1.2rem; margin-left: 1rem;">
+              Home
+            </RouterLink>
+      
+
+    
+
+  </nav>
+</template>
+<style >
+li {
+  list-style: none;
+  text-decoration: none;
+
+}
+</style>
