328caaf6d4d365fddc61fd88593e6c43d32bae1e
joaosantos28-dotcom <joao_santos28@estudante.sesisenai.org.br>
2026-07-02 00:23:34 -0300

    Update HomeView.vue
    Escalaaa

--- frontend/GeoBras-FrontEnd/src/views/HomeView.vue
+++ frontend/GeoBras-FrontEnd/src/views/HomeView.vue
@@ -4,6 +4,7 @@ import http from "@/http";
 import type Obra from "@/interfaces/Obra";
 import CardHome from "@/components/CardHome.vue";
 import CardBoasVindas from "@/components/CardBoasVindas.vue";
+import GraficoEscala from "@/components/GraficoEscala.vue";
 
 const obra = ref([] as Obra[]);
 </script>
@@ -16,6 +17,9 @@ const obra = ref([] as Obra[]);
   <div class="container">
     <CardHome titulo="obra" />
   </div>
+
+  <div class="container">
+    <GraficoEscala titulo="graficoE"/>
+  </div>
 </template>
 <style></style>
-
