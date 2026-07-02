75859fb68e6c3f13bd8e2ec66d689433a81836b4
joaosantos28-dotcom <joao_santos28@estudante.sesisenai.org.br>
2026-07-01 23:59:40 -0300

    Update HomeView.vue
    add

--- frontend/GeoBras-FrontEnd/src/views/HomeView.vue
+++ frontend/GeoBras-FrontEnd/src/views/HomeView.vue
@@ -3,13 +3,19 @@ import { ref, onMounted } from "vue";
 import http from "@/http";
 import type Obra from "@/interfaces/Obra";
 import CardHome from "@/components/CardHome.vue";
+import CardBoasVindas from "@/components/CardBoasVindas.vue";
 
 const obra = ref([] as Obra[]);
 </script>
 
 <template>
+<div class="container">
+  <CardBoasVindas titulo="boasVindas"/>
+</div>
+
   <div class="container">
     <CardHome titulo="obra" />
   </div>
 </template>
 <style></style>
+
